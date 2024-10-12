abbc64e9ff18d283b4612caffb77adc8b63c2374
SAHU <161798305+viratvijay143@users.noreply.github.com>
2024-10-12 18:33:12 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,10 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TG_API_ID", 1234))
-    API_HASH = env.get("TG_API_HASH", "xyz")
-    BOT_TOKEN = env.get("TG_BOT_TOKEN", "abc")
-    BOT_USERNAME = env.get("TG_BOT_USERNAME", "DrReactBot")
+    API_ID = int(env.get("TG_API_ID", 20047839))
+    API_HASH = env.get("TG_API_HASH", "e635f85a4dae812a26c450c0d41276b0")
+    BOT_TOKEN = env.get("TG_BOT_TOKEN", "7556329420:AAF_2EVvbuijrhItTAkwK9ee3bL14IINvHk")
+    BOT_USERNAME = env.get("TG_BOT_USERNAME", "Reactoin_haibot")
     EMOJIS = [
         "👍", "👎", "❤", "🔥", 
         "🥰", "👏", "😁", "🤔",
